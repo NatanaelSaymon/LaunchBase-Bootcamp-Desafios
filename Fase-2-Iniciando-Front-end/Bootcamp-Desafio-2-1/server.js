555bc526f5ef683fcb8cd4b6ef4f1e8ac4e8eb4e
Natanael Saymon <nsaymonsouza@gmail.com>
2020-03-17 11:29:44 -0300

    transformando arquivos html em njk

--- Fase-2-Iniciando-Front-end/Bootcamp-Desafio-2-1/server.js
+++ Fase-2-Iniciando-Front-end/Bootcamp-Desafio-2-1/server.js
@@ -8,14 +8,14 @@ const server = express()
 
 server.use(express.static('public'))
 
-server.set('view engine', 'html')
+server.set('view engine', 'njk')
 
 server.listen(5000,function(){
     console.log('Server is runnign')
 })
 
 server.get("/",function(req, res){
-    return res.render("index")
+    return res.render("layout")
 })
 
 server.get("/courses",function(req, res){
