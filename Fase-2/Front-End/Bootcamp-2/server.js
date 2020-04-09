9857835ee21a0914fe48fb31de92eb3996f4d735
Natanael Saymon <nsaymonsouza@gmail.com>
2020-04-09 15:10:27 -0300

    modificações no bootcamp 2 concluidas.

--- Fase-2/Front-End/Bootcamp-2/server.js
+++ Fase-2/Front-End/Bootcamp-2/server.js
@@ -1,19 +1,15 @@
-//http://localhost:5000/
-
 const express = require('express')
-
 const nunjucks = require('nunjucks')
-
 const server = express()
 
 server.use(express.static('public'))
-
 server.set('view engine', 'njk')
 
-server.listen(5000,function(){
-    console.log('Server is runnign')
+nunjucks.configure("views", {
+    express:server
 })
 
+/* ROTAS */
 server.get("/",function(req, res){
     return res.render("index")
 })
@@ -30,6 +26,8 @@ server.use(function(req, res) {
     res.status(404).render("not-found");
 });
 
-nunjucks.configure("views", {
-    express:server
+/* ACESSAR: http://localhost:5000/ */
+server.listen(5000,function(){ 
+    console.log('Server is runnign')
 })
+
